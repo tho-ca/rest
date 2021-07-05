fe57693bfc28207ffdf0752e4edfef5a8b47cfd4
caiodomingues <caiodss99@hotmail.com>
2021-07-04 22:45:28 -0300

    docs: base docs

--- src/stubs/AbstractRepository.php
+++ src/stubs/AbstractRepository.php
@@ -6,14 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class AbstractRepository
 {
+    /** @var Model */
     protected $model;
 
+    /**
+     * Constructor
+     *
+     * @param Model $model
+     */
     public function __construct(Model $model)
     {
         $this->model = $model;
     }
 
-    public function findById($id) {
+    /**
+     * Find model by id
+     *
+     * @param number $id
+     * @return Model|Collection|Builder|Builder[]
+     */
+    public function findById($id)
+    {
         return $this->model->findOrFail($id);
     }
 }
